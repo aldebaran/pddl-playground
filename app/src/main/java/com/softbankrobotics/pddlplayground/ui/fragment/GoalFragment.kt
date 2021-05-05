20572ee9bf52cd78863794beb37ed16a651cdbe3
Shin Watanabe <shin.watanabe@softbankrobotics.com>
2021-05-05 18:09:46 +0200

    simplify goal fragment

--- app/src/main/java/com/softbankrobotics/pddlplayground/ui/fragment/GoalFragment.kt
+++ app/src/main/java/com/softbankrobotics/pddlplayground/ui/fragment/GoalFragment.kt
@@ -5,19 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
-import com.softbankrobotics.pddlplayground.R
 import com.softbankrobotics.pddlplayground.data.DatabaseHelper
 import com.softbankrobotics.pddlplayground.databinding.FragmentEditGoalBinding
 import com.softbankrobotics.pddlplayground.model.Expression
 import com.softbankrobotics.pddlplayground.service.LoadExpressionsService
 import com.softbankrobotics.pddlplayground.ui.main.MainFragment
 import com.softbankrobotics.pddlplayground.ui.main.MainFragment.Companion.EDIT_EXPRESSION
-import com.softbankrobotics.pddlplayground.util.PDDLCategory
-import timber.log.Timber
 
 class GoalFragment: DialogFragment() {
     companion object {
@@ -33,8 +27,6 @@ class GoalFragment: DialogFragment() {
 
     private var goal: Expression? = null
     private var action: String? = null
-    private val inits = mutableListOf<String?>()
-    private var manual = false
 
     private var _binding: FragmentEditGoalBinding? = null
     private val binding get() = _binding!!
@@ -58,98 +50,13 @@ class GoalFragment: DialogFragment() {
         goal = arguments?.getParcelable("expression_extra")
         action = arguments?.getString("action")
 
-        // get view elements
-        val keywordSpinner = binding.keywordSpinner
-        val initSpinner = binding.initSpinner
-        val initSpinner2 = binding.initSpinner2
-
-        // populate keyword spinner
-        val keywords = listOf(
-            "",
-            "imply" //TODO: add more e.g. exists, forall
-        )
-        keywordSpinner.adapter =
-            ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, keywords)
-
         if (goal != null) { // if filled out before
-            // fill in keyword
-            val keyword = goal?.getLabel()?.substringBefore(' ')
-            if (keywords.any { it == keyword }) {
-                keywordSpinner.setSelection(keywords.indexOf(keyword))
-            }
-
-            // recover inits from Database and add negation
-            val init  = DatabaseHelper.getInstance(context!!).getExpressions()
-                .filter { it.getCategory() == PDDLCategory.INIT.ordinal }
-                .map { it.getLabel() }
-            init.forEach {
-                inits.add(it)
-                inits.add("not($it)")
-            }
-
-            // fill in
-            when (keyword) {
-                "imply" -> {
-                    initSpinner.adapter =
-                        ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, inits)
-                    initSpinner2.adapter =
-                        ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, inits)
-                    binding.expressionLayout.visibility = View.VISIBLE
-                    binding.expressionLayout2.visibility = View.VISIBLE
-                }
-                else -> { // for now, just null
-                    initSpinner.adapter =
-                        ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, inits)
-                    binding.expressionLayout.visibility = View.VISIBLE
-                    binding.expressionLayout2.visibility = View.GONE
-                }
-            }
-        }
-
-        // set the object adapter(s) depending on the predicate chosen
-        keywordSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                Timber.i("Nothing selected.")
-            }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                when (keywords[position]) {
-                    "imply" -> {
-                        initSpinner.adapter =
-                            ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, inits)
-                        initSpinner2.adapter =
-                            ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, inits)
-                        binding.expressionLayout.visibility = View.VISIBLE
-                        binding.expressionLayout2.visibility = View.VISIBLE
-                    }
-                    else -> { // for now, just null
-                        initSpinner.adapter =
-                            ArrayAdapter(context!!,  R.layout.support_simple_spinner_dropdown_item, inits)
-                        binding.expressionLayout.visibility = View.VISIBLE
-                        binding.expressionLayout2.visibility = View.GONE
-                    }
-                }
-            }
+            binding.goalText.setText(goal?.getLabel())
         }
 
         binding.okButton.setOnClickListener {
             goal?.apply {
-                var expression: String
-                if (manual) {
-                    expression = binding.goalText.text.toString()
-                } else {
-                    expression = keywordSpinner.selectedItem as String
-                    if (binding.expressionLayout.visibility == View.VISIBLE)
-                        expression += " (${initSpinner.selectedItem})"
-                    if (binding.expressionLayout2.visibility == View.VISIBLE)
-                        expression += " (${initSpinner2.selectedItem})"
-                }
-                setLabel(expression)
+                setLabel(binding.goalText.text.toString())
                 DatabaseHelper.getInstance(context!!).updateExpression(this)
                 LoadExpressionsService.launchLoadExpressionsService(context!!)
                 action = EDIT_EXPRESSION
@@ -159,13 +66,6 @@ class GoalFragment: DialogFragment() {
 
         binding.cancelButton.setOnClickListener {
             dismiss()
-        }
-
-        binding.manualButton.setOnClickListener {
-            binding.autoFillGoal.visibility = View.GONE
-            binding.goalText.visibility = View.VISIBLE
-            binding.goalText.setText(goal?.getLabel())
-            manual = true
         }
     }
 
